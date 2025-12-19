1119204e4b07f5895bc9867fff395554594e4116
Sergey Ukustov <sergey@ukstv.me>
2025-12-19 15:13:22 +0300

    comments

--- src/main.rs
+++ src/main.rs
@@ -101,7 +101,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-// For EOA wallets, parse signature into v, r, s and use that overload
-// For smart wallets, use the bytes signature overload
-
-// Verify that the requested feePayer is managed by this facilitator
+// TODO For EOA wallets, parse signature into v, r, s and use that overload
+// TODO For smart wallets, use the bytes signature overload
