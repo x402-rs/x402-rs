f4c7739b47ccf6df63eee8b4dc233f0748248f59
Sergey Ukustov <sergey@ukstv.me>
2026-02-07 02:58:59 +0300

    wip

--- crates/chains/x402-chain-eip155/src/v2_eip155_exact/facilitator/permit2.rs
+++ crates/chains/x402-chain-eip155/src/v2_eip155_exact/facilitator/permit2.rs
@@ -1,4 +1,5 @@
 use alloy_primitives::{Address, address};
+use alloy_provider::Provider;
 use x402_types::chain::ChainProviderOps;
 use x402_types::proto::{PaymentVerificationError, v2};
 use x402_types::scheme::X402SchemeFacilitatorError;
@@ -6,10 +7,12 @@ use x402_types::scheme::X402SchemeFacilitatorError;
 #[cfg(feature = "telemetry")]
 use tracing::instrument;
 
-use crate::chain::Eip155MetaTransactionProvider;
+use crate::chain::{Eip155ChainReference, Eip155MetaTransactionProvider};
 use crate::v1_eip155_exact::{assert_enough_value, assert_time};
 use crate::v2_eip155_exact::eip3009::assert_requirements_match;
-use crate::v2_eip155_exact::{Permit2PaymentPayload, Permit2PaymentRequirements};
+use crate::v2_eip155_exact::types::{
+    Permit2Payload, Permit2PaymentPayload, Permit2PaymentRequirements,
+};
 
 pub const EXACT_PERMIT2_PROXY_ADDRESS: Address =
     address!("0x4020615294c913F045dc10f0a5cdEbd86c280001");
@@ -24,33 +27,12 @@ pub async fn verify_permit2_payment<P: Eip155MetaTransactionProvider + ChainProv
     let accepted = &payment_payload.accepted;
     assert_requirements_match(accepted, payment_requirements)?;
 
-    // Spender must be the x402ExactPermit2Proxy contract address
-    let authorization = &payment_payload.payload.permit_2_authorization;
-    if authorization.spender.0 != EXACT_PERMIT2_PROXY_ADDRESS {
-        return Err(PaymentVerificationError::RecipientMismatch);
-    }
-
-    // Correct recipient
-    let witness = &authorization.witness;
-    if witness.to != payment_requirements.pay_to {
-        return Err(PaymentVerificationError::RecipientMismatch);
-    }
-
-    // Time validity
-    let valid_after = witness.valid_after;
-    let valid_before = authorization.deadline;
-    assert_time(valid_after, valid_before)?;
-
-    // Sufficient amount
-    let amount_required = &payment_requirements.amount;
-    assert_enough_value(&authorization.permitted.amount, amount_required)?;
-
-    // Same token
-    if authorization.permitted.token != payment_requirements.asset {
-        return Err(PaymentVerificationError::AssetMismatch);
-    }
-
-    println!("---- Permit2 - verify_permit2_payment");
+    assert_valid_payment(
+        provider.inner(),
+        provider.chain(),
+        accepted,
+        &payment_payload.payload,
+    )?;
 
     todo!("Permit2 - verify_permit2_payment")
 }
@@ -62,4 +44,41 @@ pub async fn settle_permit2_payment<P: Eip155MetaTransactionProvider + ChainProv
     payment_requirements: &Permit2PaymentRequirements,
 ) -> Result<v2::SettleResponse, X402SchemeFacilitatorError> {
     todo!("Permit2 - settle_permit2_payment")
+}
+
+pub fn assert_valid_payment<P: Provider>(
+    provider: P,
+    chain: &Eip155ChainReference,
+    accepted: &Permit2PaymentRequirements,
+    payload: &Permit2Payload,
+) -> Result<(), PaymentVerificationError> {
+    // Spender must be the x402ExactPermit2Proxy contract address
+    let authorization = &payload.permit_2_authorization;
+    if authorization.spender.0 != EXACT_PERMIT2_PROXY_ADDRESS {
+        return Err(PaymentVerificationError::RecipientMismatch);
+    }
+
+    // Correct recipient
+    let witness = &authorization.witness;
+    if witness.to != accepted.pay_to {
+        return Err(PaymentVerificationError::RecipientMismatch);
+    }
+
+    // Time validity
+    let valid_after = witness.valid_after;
+    let valid_before = authorization.deadline;
+    assert_time(valid_after, valid_before)?;
+
+    // Sufficient amount
+    let amount_required = &accepted.amount;
+    assert_enough_value(&authorization.permitted.amount, amount_required)?;
+
+    // Same token
+    if authorization.permitted.token != accepted.asset {
+        return Err(PaymentVerificationError::AssetMismatch);
+    }
+
+    println!("---- Permit2 - verify_permit2_payment"); // TODO Continue
+
+    Ok(())
 }
