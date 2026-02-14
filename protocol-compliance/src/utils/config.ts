2185b07cbd7cb7abf64b350d38174f23c1b70270
Sergey Ukustov <sergey@ukstv.me>
2026-02-15 01:02:42 +0300

    wip

--- protocol-compliance/src/utils/config.ts
+++ protocol-compliance/src/utils/config.ts
@@ -72,6 +72,10 @@ export const FACILITATOR_CONFIG = {
       chains: "eip155:*",
     },
     {
+      id: "v2-eip155-upto",
+      chains: "eip155:*",
+    },
+    {
       id: "v1-solana-exact",
       chains: "solana:*",
     },
