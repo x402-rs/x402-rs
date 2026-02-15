4b01add1930aa402bdbd0c880445b5df062359a7
Sergey Ukustov <sergey@ukstv.me>
2026-02-15 13:59:33 +0300

    Revert "after merge"
    This reverts commit 7f4ece4f6649435b1d77e027adb20bfbcef42264.

--- crates/chains/x402-chain-eip155/src/v2_eip155_exact/facilitator/eip3009.rs
+++ crates/chains/x402-chain-eip155/src/v2_eip155_exact/facilitator/eip3009.rs
@@ -108,7 +108,7 @@ pub async fn assert_valid_payment<P: Provider>(
     let payment = ExactEvmPayment {
         from: authorization.from,
         to: authorization.to,
-        value: authorization.value.into(),
+        value: authorization.value,
         valid_after: authorization.valid_after,
         valid_before: authorization.valid_before,
         nonce: authorization.nonce,
